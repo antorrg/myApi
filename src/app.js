1e53286d99095860c469faf0ba2067c06aeafa83
antorrg <114789901+antorrg@users.noreply.github.com>
2024-06-20 09:56:42 -0300

    updated to ejs

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(morgan('dev'))
 // }))
 app.use(cors())
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
+app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, 'views')))
 app.use(express.static(path.join(__dirname, "public")));
 app.use('/detalles', express.static(path.join(__dirname, 'public')));
