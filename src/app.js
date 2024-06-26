eeafc201534134ffa23c63fe36e161b263fc5a0b
antorrg <114789901+antorrg@users.noreply.github.com>
2024-06-25 23:15:27 -0300

    commit corrections

--- src/app.js
+++ src/app.js
@@ -12,25 +12,25 @@ const __dirname = path.dirname(__filename);
 //const __dirname = path.dirname(new URL(import.meta.url).pathname);
 //console.log(__dirname)
 const app = express();
-app.use((req, res, next) => {
-    res.locals.nonce = crypto.randomBytes(16).toString('hex');
-    console.log('Generated Nonce:', res.locals.nonce); 
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.locals.nonce = crypto.randomBytes(16).toString('hex');
+//     console.log('Generated Nonce:', res.locals.nonce); 
+//     next();
+//   });
 
 app.use(morgan('dev'))
-app.use(helmet({
-        contentSecurityPolicy: {
-            useDefaults: true,
-            directives: {
-                defaultSrc: ["'self'"],
-                scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
-                imgSrc: ["'self'", 'https://firebasestorage.googleapis.com', 'data:'],
-                // Agregar otras directivas según sea necesario
-            },
-        },
-    })
-);
+// app.use(helmet({
+//         contentSecurityPolicy: {
+//             useDefaults: true,
+//             directives: {
+//                 defaultSrc: ["'self'"],
+//                 //scriptSrc: ["'self'", (req, res) => `'nonce-${res.locals.nonce}'`],
+//                 imgSrc: ["'self'", 'https://firebasestorage.googleapis.com', 'data:'],
+//                 // Agregar otras directivas según sea necesario
+//             },
+//         },
+//     })
+// );
 
 app.use(cors())
 app.set("views", path.join(__dirname, "views"));
